c183815dd0dc03f748d8c42b26c2e0dc183d0552
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-09-07 11:50:19 +0900

    first commit

---
+++ MySlotMachine/code/main.js
@@ -0,0 +1,61 @@
+'use strict'
+{
+  const createElement = (imgID, stopID) => {
+    const divElement = document.createElement('div')
+    divElement.classList.add('slot-panel')
+
+    const img = document.createElement('img')
+    img.src = 'bell.png'
+    img.setAttribute('id', imgID)
+
+    const stop = document.createElement('button')
+    stop.setAttribute('id', stopID)
+    stop.textContent = 'STOP'
+
+    divElement.appendChild(img)
+    divElement.appendChild(stop)
+
+    const slotBodyElement = document.querySelector('div')
+    slotBodyElement.appendChild(divElement)
+  }
+
+  createElement('img1', 'stop1')
+  createElement('img2', 'stop2')
+  createElement('img3', 'stop3')
+
+  const img1 = document.getElementById('img1')
+  const img2 = document.getElementById('img2')
+  const img3 = document.getElementById('img3')
+
+  const spin = document.getElementById('spin')
+  const stop1 = document.getElementById('stop1')
+  const stop2 = document.getElementById('stop2')
+  const stop3 = document.getElementById('stop3')
+
+  spin.addEventListener('click', () => {
+    let intervalID1 = spinningSlot(img1)
+    let intervalID2 = spinningSlot(img2)
+    let intervalID3 = spinningSlot(img3)
+    addStopProcess(stop1, intervalID1)
+    addStopProcess(stop2, intervalID2)
+    addStopProcess(stop3, intervalID3)
+  })
+
+  const spinningSlot = (img) => {
+    let intervalID = setInterval(() => {
+      img.src = switchImage()
+    }, 10)
+    return intervalID
+  }
+
+  const switchImage = () => {
+    const imgArray = ['cherry.png', 'bell.png', 'seven.png']
+    return imgArray[Math.floor(Math.random() * imgArray.length)]
+  }
+
+  const addStopProcess = (element, intervalID) => {
+    element.addEventListener('click', () => {
+      clearInterval(intervalID)
+    })
+  }
+}
