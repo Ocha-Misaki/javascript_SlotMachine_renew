a2f6d5497c1fd8434831a3ed8ddd63718c0ae547
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-09-07 14:28:42 +0900

    new Slotを配列にいれ、spinを押した時に正常に動作するようにした

--- MySlotMachine/code/main.js
+++ MySlotMachine/code/main.js
@@ -1,24 +1,49 @@
 'use strict'
 {
   class Slot {
-    constructor(id, num) {
-      for (let i = 0; i < num; i++) {
-        const divElement = document.createElement('div')
-        divElement.classList.add('slot-panel')
+    constructor(id) {
+      const divElement = document.createElement('div')
+      divElement.classList.add('slot-panel')
 
-        this.img = document.createElement('img')
-        this.img.src = 'bell.png'
+      this.img = document.createElement('img')
+      this.img.src = 'bell.png'
 
-        this.stop = document.createElement('button')
-        this.stop.textContent = 'STOP'
+      this.stop = document.createElement('button')
+      this.stop.textContent = 'STOP'
 
-        divElement.appendChild(this.img)
-        divElement.appendChild(this.stop)
-        const slotBodyElement = document.getElementById(id)
-        slotBodyElement.appendChild(divElement)
-      }
+      divElement.appendChild(this.img)
+      divElement.appendChild(this.stop)
+      const slotBodyElement = document.getElementById(id)
+      slotBodyElement.appendChild(divElement)
+    }
+
+    start() {
+      const spin = document.getElementById('spin')
+      spin.addEventListener('click', () => {
+        let intervalID = this.spinningSlot(this.img)
+        this.addStopProcess(this.stop, intervalID)
+      })
+    }
+    spinningSlot(img) {
+      let intervalID = setInterval(() => {
+        img.src = this.switchImage()
+      }, 10)
+      return intervalID
+    }
+
+    switchImage = () => {
+      const imgArray = ['cherry.png', 'bell.png', 'seven.png']
+      return imgArray[Math.floor(Math.random() * imgArray.length)]
+    }
+
+    addStopProcess = (element, intervalID) => {
+      element.addEventListener('click', () => {
+        clearInterval(intervalID)
+      })
     }
   }
-
-  new Slot('slot', 6)
+  const slots = [new Slot('slot'), new Slot('slot'), new Slot('slot')]
+  slots.forEach((slot) => {
+    slot.start()
+  })
 }
